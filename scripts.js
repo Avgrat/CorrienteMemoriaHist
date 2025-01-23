0ffbda08b5503d60d471cb2d093523d6f0d09041
Av <78084509+Avgrat@users.noreply.github.com>
2025-01-23 11:38:46 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-//cosita del menu
-const menualt = document.querySelector('.menu-alt');
-const navbarder = document.querySelector('.navbar-der');
-
-menualt.addEventListener('click',() =>{
-    navbarder.classList.toggle('active');
-});
